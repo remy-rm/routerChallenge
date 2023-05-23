95c476645a8e2b02400a02bc4ce1665e3b8c71d0
remyrm <remy.marquis62121@gmail.com>
2023-05-23 12:33:11 +0200

    04 - Le Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserProfileComponent } from './menu/user-profile/user-profile.component';
+import { SignUpComponent } from './menu/sign-up/sign-up.component';
 
+
+
+const ROUTES: Routes = [
+  {path: 'signup', component: SignUpComponent},
+  {path: 'user', component: UserProfileComponent},
+  {path:'', redirectTo: '/user', pathMatch: 'full'}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignUpComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
